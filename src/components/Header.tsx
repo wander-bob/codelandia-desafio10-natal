3f15f2b3d8713d87589230f000028152fc068754
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-09-23 23:24:12 -0300

    feat: implementing link component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import '../styles/header.css';
 import logoImg from '../assets/logo.svg';
 import storeImg from '../assets/store.svg';
+import { Link } from './Link';
 
 export function Header() {
   return (
@@ -11,6 +12,7 @@ export function Header() {
           Uma <span>plataforma de doação</span> para espalhar alegria
         </h1>
         <p>Nessas festas de fim de ano mande um presente para a pessoa amada e compartilhe a alegria do Natal.</p>
+        <Link content="Fazer uma doação" />
       </div>
       <img src={storeImg} alt="Loja" />
     </header>
