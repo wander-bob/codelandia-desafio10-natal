9b7a54164e01544e2508a843aaede9d4f2f02cc2
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-09-23 23:22:34 -0300

    feat: adding header componet

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import '../styles/header.css';
+import logoImg from '../assets/logo.svg';
+import storeImg from '../assets/store.svg';
+
+export function Header() {
+  return (
+    <header>
+      <div className="content">
+        <img src={logoImg} alt="Santa Logo" />
+        <h1>
+          Uma <span>plataforma de doação</span> para espalhar alegria
+        </h1>
+        <p>Nessas festas de fim de ano mande um presente para a pessoa amada e compartilhe a alegria do Natal.</p>
+      </div>
+      <img src={storeImg} alt="Loja" />
+    </header>
+  );
+}
