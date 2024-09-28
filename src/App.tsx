8f4f684934025476214d16416197125eb6013501
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-09-28 17:28:23 -0300

    feat: Adding mobile styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Main } from './components/Main';
+import { Footer } from './components/Footer';
 
 export function App() {
   return (
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -14,12 +14,21 @@ export function Main() {
           <p>Nessas festas de fim de ano mande um presente para a pessoa amada e compartilhe a alegria do Natal.</p>
         </div>
         <h1>10d -24h - 01m - 52s</h1>
-        <img src={illustrationImg} alt="Presentes" />
+        <img
+          src={illustrationImg}
+          alt="Presentes"
+        />
       </div>
       <div className="presentation">
-        <img src={presentImg} alt="Mãos segurando um enfeite de Natal" />
+        <img
+          src={presentImg}
+          alt="Mãos segurando um enfeite de Natal"
+        />
         <div className="content">
-          <h2>Conectando generosidade ao redor do mundos</h2>
+          <h2>
+            Conectando generosidade <br />
+            ao redor do mundo
+          </h2>
           <p>
             Celebre a magia do Natal de uma maneira especial! Descubra a plataforma que permite a doação de presentes
             significativos para aqueles que mais precisam. Faça parte do movimento e compartilhe o verdadeiro espírito
